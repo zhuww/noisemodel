858fe0ee7dbdcc393ede16c197765f7ff23bda48
Weiwei Zhu <zhuww@pc20180.mpifr-bonn.mpg.de.mpifr-bonn.mpg.de.mpifr-bonn.mpg.de.mpifr-bonn.mpg.de.mpifr-bonn.mpg.de.mpifr-bonn.mpg.de.mpifr-bonn.mpg.de>
2014-10-29 00:01:13 +0100

    commit changes for modeling OMDOT_PA.

--- calOMDOT.py
+++ calOMDOT.py
@@ -20,15 +20,22 @@ Tsun = 4.925490947e-6
 #pf = model('J1713+0747_NANOGrav_8yv0.gls.par')
 #pf = model('1713.GLS.out.par')
 pf = model('J1713+0747.par')
+#pf = model('1713.Sep.T2.gls.par')
 
 M2 = float(pf.M2[0])
-SINI = float(pf.SINI[0])
+if pf.SINI == 'KIN':
+    SINI = np.sin(float(pf.KIN[0])/180.*np.pi)
+else:
+    SINI = float(pf.SINI[0])
 Pb = float(pf.PB[0]) * secperday
 a = float(pf.A1[0]) 
 #M1 = (Pb/2/pi*sqrt(G*(M2*SINI)**3/a**3)-M2)/Msun
 M1 = Pb/2/pi*((Tsun*(M2*SINI)**3/a**3)**(0.5))-M2
 #M1 = 1.4
-E = float(pf.E[0])
+try:
+    E = float(pf.E[0])
+except:
+    E = float(pf.ECC[0])
 f = 4*pi**2*a**3/Pb**2/Tsun
 print 'Timing M1,M2,Mtot,f:',M1,M2, M1+M2, f #, (M2*SINI)**3/(M1+M2)**2
 print 'SINI:', SINI
@@ -40,12 +47,15 @@ OMDOT = 3.*(2*pi/Pb)**(5./3)*(Tsun*(M1+M2))**(2./3)/(1. - E**2)
 OMDOT_PA = OMDOT * 180./pi * secperday * dayperyear
 print 'periastron advance:', OMDOT_PA  , 'degress per year'
 
-Omega = float(str(pf.PAASCNODE))
+try:
+    Omega = float(str(pf.PAASCNODE))
+except:
+    Omega = float(pf.KOM[0])
 thetamu = np.arctan(float(str(pf.PMRA[0]/pf.PMDEC[0]))) #in radian
 #thetamu = 180. + np.arctan(float(str(pf.PMRA[0]/pf.PMDEC[0])))/np.pi*180
 #print 'thetamu:', thetamu, 'Omega:',Omega
 mu = np.sqrt(float(pf.PMRA[0])**2 + float(pf.PMDEC[0])**2)
-muprime = mu/float(pf.SINI[0])
+muprime = mu/SINI
 OMDOT_SM = muprime*np.cos((thetamu-Omega*pi/180.))/60/60 /1000 
 
 print 'Thetamu - Omega:', (thetamu-Omega*pi/180.)*180./pi
@@ -60,7 +70,7 @@ Delta_DM = float(pf.OMDOT[0]) - OMDOT_PA - OMDOT_SM
 Pb = float(pf.PB[0])
 Delta_PB =  Delta_DM/360/dayperyear * Pb * Pb
 print "TO move OMDOT to GR value:", Delta_PB
-pf.PB[0] += Decimal(Delta_PB)
+pf.PB[0] -= Decimal(Delta_PB)
 pf.OMDOT[0] = Decimal(OMDOT_PA + OMDOT_SM)
 pf.write('1713.Oct.omdot.par')
 
